7aeae2e45b4f3970d1dcfe166788b4fd62dc6c4a
baek <bgh9651@gmail.com>
2023-12-28 17:20:05 +0900

    Upt. setting

---
+++ src/app/QueryProvider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function QueryProvider({ children }: Props) {
+  const [client] = React.useState(
+    new QueryClient({
+      defaultOptions: {
+        // react-query 전역 설정
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+          staleTime: 60 * 1000 * 60, // 1 hour
+          //   suspense: true,
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
---
+++ src/app/error.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const Error = () => {
+  const router = useRouter();
+
+  return (
+    <div className="w-full h-full flex justify-center flex-col items-center">
+      <h2 className="my-[40px] text-2xl">페이지를 찾을수 없습니다.</h2>
+      <div>
+        <Button
+          size="lg"
+          color="red"
+          className="rounded-md md:inline-block text-lg"
+          onClick={() => router.push("/")}
+        >
+          메인 페이지로 돌아가기
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
